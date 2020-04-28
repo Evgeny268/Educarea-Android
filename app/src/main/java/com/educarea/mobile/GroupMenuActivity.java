1dc509e1364d047251005c7004276648b9a70688
Evgeny268 <evgepishcin@gmail.com>
2020-04-28 19:11:35 +0300

    delete old import

--- app/src/main/java/com/educarea/mobile/GroupMenuActivity.java
+++ app/src/main/java/com/educarea/mobile/GroupMenuActivity.java
@@ -1,12 +1,8 @@
 package com.educarea.mobile;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
